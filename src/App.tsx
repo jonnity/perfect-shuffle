aeb74c1cf06083acf227670cc3a5e6dce91953c3
jonnity <jonny1996_ty@yahoo.co.jp>
2025-11-16 14:29:57 +0000

    App.tsxのスタイルを調整

--- src/App.tsx
+++ src/App.tsx
@@ -29,13 +29,11 @@ function App() {
   return (
     <BrowserRouter>
       <PageViewTracker />
-      <div className="pb-32">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/shuffle" element={<ShufflePage />} />
-          <Route path="/complete" element={<CompletePage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shuffle" element={<ShufflePage />} />
+        <Route path="/complete" element={<CompletePage />} />
+      </Routes>
       <Footer />
       <AdBanner />
     </BrowserRouter>
